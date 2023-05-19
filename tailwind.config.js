62d3f2a862c3003533668746c783cf6e19b21d51
Appiah Boateng <info.ystylist@gmail.com>
2023-05-19 17:03:22 -0400

    new commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
              "color-primary":"#01051e",
              "color-primary-light": "#020726",
              "color-primary-dark": "#010417",
-             "color-secondary": "#ff7d3b",
+             "color-secondary": "#36B5EE",
              "color-gray": "#333",
              "color-white": "#fff",
              "color-blob": "#A427DF",
@@ -22,7 +22,7 @@ module.exports = {
       }
     },
     fontFamily:{
-      'sans': ['Poppins', 'sans-serif'],
+      'sans': ['Figtree', 'sans-serif'],
       'mono': ['Montserrat', 'sans-serif'],
       'cursive':['Rubik Pixels', 'cursive']
 
